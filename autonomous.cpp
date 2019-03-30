b7670ea32a7f7715bc6f8c76b6224ad1b3bd1331
team81k <team81k@gmail.com>
2019-03-30 01:54:07 -0400

    autonomous base power calculation

--- autonomous.cpp
+++ autonomous.cpp
@@ -8,52 +8,65 @@ double driveThreshold = 0;
 
 void autonomousAsync(void * param)
 {
-    if(driveType == 0)
-    {
-        FR.move(0);
-        BR.move(0);
-        FL.move(0);
-        BL.move(0);
-    }
-    else if(driveType == 1)
+    double rightPower = 0;
+    double leftPower = 0;
+
+    if(driveType == 1)
     {
         double avgDistance = (FR.get_position() + BR.get_position() + FL.get_position() + BL.get_position()) / 4;
 
-        FR.move(forwardDrivePID.calculate(avgDistance));
-        BR.move(forwardDrivePID.getPower());
-        FL.move(forwardDrivePID.getPower());
-        BL.move(forwardDrivePID.getPower());
+        rightPower = forwardDrivePID.calculate(avgDistance);
+        leftPower = forwardDrivePID.getPower();
     }
     else if(driveType == 2)
     {
         double avgDistance = (FR.get_position() + BR.get_position() - FL.get_position() - BL.get_position()) / 4;
 
-        FR.move(turnDrivePID.calculate(avgDistance));
-        BR.move(turnDrivePID.getPower());
-        FL.move(-turnDrivePID.getPower());
-        BL.move(-turnDrivePID.getPower());
+        rightPower = turnDrivePID.calculate(avgDistance);
+        leftPower = -turnDrivePID.getPower();
     }
+
+    double differentialPower = differentialPID.calculate(differentialPot.get_value());
+
+    double FRP = rightPower + differentialPower;
+    double BRP = rightPower - differentialPower;
+    double FLP = leftPower + differentialPower;
+    double BLP = leftPower - differentialPower;
+
+    if(FRP > 127) { BRP -= FRP - 127; FRP = 127; }
+    if(BRP > 127) { FRP -= BRP - 127; BRP = 127; }
+    if(FRP < -127) { BRP -= FRP + 127; FRP = -127; }
+    if(BRP < -127) { FRP -= BRP + 127; BRP = -127; }
+
+    if(FLP > 127) { BLP -= FLP - 127; FLP = 127; }
+    if(BLP > 127) { FLP -= BLP - 127; BLP = 127; }
+    if(FLP < -127) { BLP -= FLP + 127; FLP = -127; }
+    if(BLP < -127) { FLP -= BLP + 127; BLP = -127; }
+
+    FR.move(FRP);
+    BR.move(BRP);
+    FL.move(FLP);
+    BL.move(BLP);
 }
 
-/*
-void driveStrait(double meters, bool wait = true, long timeout = -1);
-void driveTurn(double radians, bool wait = true, long timeout = -1);
-void waitOnDrive();
-void driveStop();
-void flywheelSpeed(double speed, bool wait = true, long timeout = -1);
-void waitOnFlywheel();
-void intakeIn();
-void launchBall(bool wait =  true, long timeout = -1);
-*/
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void driveStrait(double meters, bool wait = true, long timeout = -1)
+void driveTransform(double position, bool wait = true, long timeout = -1)
+{
+    differentialPID.target = position;
+    long waitStart = pros::millis();
+    if(wait) while(fabs(differentialPID.getError()) > 25 && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
+}
+
+void driveStraight(double meters, bool wait = true, long timeout = -1)
 {
     driveType = 1;
     FR.tare_position();BR.tare_position();FL.tare_position();BL.tare_position();
     forwardDrivePID.clear();
     forwardDrivePID.target = (meters / wheelCircumference) / gearRatio * ticksPerRevolution;
     driveThreshold = 50;
-    if(wait) while(fabs(forwardDrivePID.getError()) > driveThreshold) pros::delay(3);
+    long waitStart = pros::millis();
+    if(wait) while(fabs(forwardDrivePID.getError()) > driveThreshold && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
 }
 
 void driveTurn(double radians, bool wait = true, long timeout = -1)
@@ -64,13 +77,15 @@ void driveTurn(double radians, bool wait = true, long timeout = -1)
     double inches = (PI * wheelWidth) * (radians / TAU);
     turnDrivePID.target = (inches / wheelCircumference) / gearRatio * ticksPerRevolution;
     driveThreshold = 50;
-    if(wait) while(fabs(turnDrivePID.getError()) > driveThreshold) pros::delay(3);
+    long waitStart = pros::millis();
+    if(wait) while(fabs(turnDrivePID.getError()) > driveThreshold && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
 }
 
-void waitOnDrive()
+void waitOnDrive(long timeout = -1)
 {
-    if(driveType == 1) while(fabs(forwardDrivePID.getError()) > driveThreshold) pros::delay(3);
-    else if(driveType == 2) while(fabs(turnDrivePID.getError()) > driveThreshold) pros::delay(3);
+    long waitStart = pros::millis();
+    if(driveType == 1) while((fabs(forwardDrivePID.getError()) || fabs(differentialPID.getError()) > 25) > driveThreshold && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
+    else if(driveType == 2) while((fabs(turnDrivePID.getError()) || fabs(differentialPID.getError()) > 25) > driveThreshold && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
 }
 
 void driveStop() { driveType = 0; }
@@ -81,13 +96,25 @@ void driveStop() { driveType = 0; }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*
+void driveTransform(double position, bool wait = true, long timeout = -1);
+void driveStraight(double meters, bool wait = true, long timeout = -1);
+void driveTurn(double radians, bool wait = true, long timeout = -1);
+void waitOnDrive(long timeout = -1);
+void driveStop();
+void flywheelSpeed(double speed, bool wait = true, long timeout = -1);
+void waitOnFlywheel();
+void intakeIn();
+void launchBall(bool wait =  true, long timeout = -1);
+*/
+
 void autonomous()
 {
     pros::task_t autonomousAsyncTask = pros::c::task_create(autonomousAsync, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "autonomousAsync");
 
     for(int i = 0; i < 8; i++)
     {
-        driveStrait(10_in);
+        driveStraight(10_in);
         driveTurn(90_deg);
     }
 
