962401693552f0881462ede843526615a902dccd
team81k <team81k@gmail.com>
2019-03-31 14:30:12 -0400

    fix autonomous functions

--- autonomous.cpp
+++ autonomous.cpp
@@ -5,6 +5,7 @@
 
 int driveType = 0;
 double driveThreshold = 0;
+bool autonPIDReady = false;
 
 void autonomousAsync(void * param)
 {
@@ -47,6 +48,8 @@ void autonomousAsync(void * param)
     BR.move(BRP);
     FL.move(FLP);
     BL.move(BLP);
+
+    autonPIDReady = true;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -54,8 +57,9 @@ void autonomousAsync(void * param)
 void driveTransform(double position, bool wait = true, long timeout = -1)
 {
     differentialPID.target = position;
+    autonPIDReady = false;
     long waitStart = pros::millis();
-    if(wait) while(fabs(differentialPID.getError()) > 25 && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
+    if(wait) while((fabs(differentialPID.getError()) > 25 || !autonPIDReady) && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
 }
 
 void driveStraight(double meters, bool wait = true, long timeout = -1)
@@ -65,8 +69,9 @@ void driveStraight(double meters, bool wait = true, long timeout = -1)
     forwardDrivePID.clear();
     forwardDrivePID.target = (meters / wheelCircumference) / gearRatio * ticksPerRevolution;
     driveThreshold = 50;
+    autonPIDReady = false;
     long waitStart = pros::millis();
-    if(wait) while(fabs(forwardDrivePID.getError()) > driveThreshold && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
+    if(wait) while((fabs(forwardDrivePID.getError()) > driveThreshold || !autonPIDReady) && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
 }
 
 void driveTurn(double radians, bool wait = true, long timeout = -1)
@@ -77,15 +82,16 @@ void driveTurn(double radians, bool wait = true, long timeout = -1)
     double inches = (PI * wheelWidth) * (radians / TAU);
     turnDrivePID.target = (inches / wheelCircumference) / gearRatio * ticksPerRevolution;
     driveThreshold = 50;
+    autonPIDReady = false;
     long waitStart = pros::millis();
-    if(wait) while(fabs(turnDrivePID.getError()) > driveThreshold && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
+    if(wait) while((fabs(turnDrivePID.getError()) > driveThreshold || !autonPIDReady) && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
 }
 
 void waitOnDrive(long timeout = -1)
 {
     long waitStart = pros::millis();
-    if(driveType == 1) while((fabs(forwardDrivePID.getError()) || fabs(differentialPID.getError()) > 25) > driveThreshold && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
-    else if(driveType == 2) while((fabs(turnDrivePID.getError()) || fabs(differentialPID.getError()) > 25) > driveThreshold && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
+    if(driveType == 1) while((fabs(forwardDrivePID.getError()) > driveThreshold || fabs(differentialPID.getError()) > 25 || !autonPIDReady) && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
+    else if(driveType == 2) while((fabs(turnDrivePID.getError()) > driveThreshold || fabs(differentialPID.getError()) > 25 || !autonPIDReady) && (timeout == -1 || pros::millis() - waitStart < timeout)) pros::delay(3);
 }
 
 void driveStop() { driveType = 0; }
@@ -110,6 +116,10 @@ void launchBall(bool wait =  true, long timeout = -1);
 
 void autonomous()
 {
+    setNavigation(false);
+	setPage(0);
+    lv_label_set_text(homeTextObject, "");
+
     pros::task_t autonomousAsyncTask = pros::c::task_create(autonomousAsync, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "autonomousAsync");
 
     for(int i = 0; i < 8; i++)
